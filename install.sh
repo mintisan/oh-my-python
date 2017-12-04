bde8d6dacbd08dcaf392fb334bfbc01be2cd6403
Jinhui-Lin <fovwin@gmail.com>
2017-12-04 17:16:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ sudo pip install numpy scipy matplotlib pandas
 sudo pip install tensorflow
 pip3 install http://download.pytorch.org/whl/cu75/torch-0.2.0.post3-cp35-cp35m-manylinux1_x86_64.whl 
 pip3 install torchvision
+sudo pip install keras
 
 ## utility
 sudo pip install tldr
